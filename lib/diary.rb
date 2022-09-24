0803f7bdb23e51e546fce0045a61c752e2794f36
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-09-24 14:20:10 +0100

    completed exercises in p3

---
+++ lib/diary.rb
@@ -0,0 +1,21 @@
+class Diary
+  def initialize
+    @entries = []
+  end
+
+  def add (entry)
+    @entries << entry
+  end
+
+  def entries
+    return @entries
+  end
+
+  def count_words
+    return @entries.sum(&:count_words)
+    end
+
+  end
+
+
+
